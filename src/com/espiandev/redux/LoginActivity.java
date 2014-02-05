99d40a990ccbfa80c2350112704c513bf114fe3b
Alex Curran <aml.curran@gmail.com>
2014-02-05 20:09:20 +0000

    Log-in activity with username validation

--- src/com/espiandev/redux/LoginActivity.java
+++ src/com/espiandev/redux/LoginActivity.java
@@ -2,12 +2,44 @@ package com.espiandev.redux;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private TextView errorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameField = (EditText) findViewById(R.id.login_username);
+        passwordField = (EditText) findViewById(R.id.login_password);
+        errorView = (TextView) findViewById(R.id.login_error);
+        findViewById(R.id.login_submit).setOnClickListener(mOnClickListener);
+
     }
+
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            if (usernameIsValid()) {
+
+            } else {
+                errorView.setText(R.string.login_error_username);
+            }
+        }
+
+    };
+
+    private boolean usernameIsValid() {
+        return !TextUtils.isEmpty(usernameField.getText());
+    }
+
 }
--- test/com/espiandev/redux/LoginActivityTest.java
+++ test/com/espiandev/redux/LoginActivityTest.java
@@ -1,6 +1,48 @@
 package com.espiandev.redux;
 
-import junit.framework.TestCase;
+import android.widget.TextView;
 
-public class LoginActivityTest extends TestCase {
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = "AndroidManifest.xml")
+public class LoginActivityTest {
+
+    private ActivityController<LoginActivity> activityController;
+
+    @Before
+    public void setUp() {
+        activityController = Robolectric.buildActivity(LoginActivity.class);
+    }
+
+    @Test
+    @Ignore
+    public void testWhenActivityStarted_UsernameFieldHasFocus() {
+        LoginActivity activity = activityController.create().start().resume().get();
+
+        assertEquals(activity.findViewById(R.id.login_username), activity.getCurrentFocus());
+    }
+
+    @Test
+    public void testWhenLoginClicked_AndUsernameIsEmpty_AnErrorIsShown() {
+        LoginActivity activity = activityController.create().start().resume().get();
+
+        activity.findViewById(R.id.login_submit).performClick();
+
+        String expectedString = activity.getString(R.string.login_error_username);
+        String actualString = String.valueOf(((TextView) activity.findViewById(R.id.login_error))
+                .getText());
+
+        assertEquals(expectedString, actualString);
+    }
+
 }
