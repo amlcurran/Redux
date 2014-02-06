67912314f0410be95166b0df2eb6ce9c1d93b6c6
Alex Curran <aml.curran@gmail.com>
2014-02-06 15:26:09 +0000

    Styled buttons blue

--- src/com/espiandev/redux/LaunchActivity.java
+++ src/com/espiandev/redux/LaunchActivity.java
@@ -10,6 +10,7 @@ public class LaunchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
         if (!hasAuthToken()) {
             startActivity(new Intent(this, LoginActivity.class));
         }
