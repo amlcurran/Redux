75ef1b889fba88b4ff9138a9c88cf3b7bcdde2f7
Alex Curran <aml.curran@gmail.com>
2014-02-07 00:44:50 +0000

    BROKEN Providers

---
+++ src/com/espiandev/redux/AnimationFactoryProvider.java
@@ -0,0 +1,7 @@
+package com.espiandev.redux;
+
+import com.espiandev.redux.animation.AnimationFactory;
+
+public interface AnimationFactoryProvider {
+    AnimationFactory getAnimationFactory();
+}
---
+++ src/com/espiandev/redux/VolleyHelperProvider.java
@@ -0,0 +1,5 @@
+package com.espiandev.redux;
+
+public interface VolleyHelperProvider {
+    VolleyHelper getVolleyHelper();
+}
