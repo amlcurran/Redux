2b922a20de457be8ee0dff42962012376cb17530
Alex Curran <aml.curran@gmail.com>
2014-02-10 15:50:56 +0000

    Improved image scaling on details fragment

---
+++ src/com/espiandev/redux/FixedImageView.java
@@ -0,0 +1,37 @@
+package com.espiandev.redux;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Alex Curran on 10/02/2014.
+ */
+public class FixedImageView extends ImageView {
+
+    public FixedImageView(Context context) {
+        super(context);
+    }
+
+    public FixedImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FixedImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Try for a width based on our minimum
+        int minw = getPaddingLeft() + getPaddingRight() + getSuggestedMinimumWidth();
+        int w = resolveSizeAndState(minw, widthMeasureSpec, 1);
+
+        // Whatever the width ends up being, ask for a height that would let the pie
+        // get as big as it can
+        int h = (int) (w / 16f) * 9;
+
+        setMeasuredDimension(w, h);
+    }
+
+}
