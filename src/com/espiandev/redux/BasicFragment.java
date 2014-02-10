1f55a6b9c4c58064a84521ac3e1ee99afce75f10
Alex Curran <aml.curran@gmail.com>
2014-02-10 19:56:36 +0000

    Collapsed extra class

--- src/com/espiandev/redux/BasicFragment.java
+++ src/com/espiandev/redux/BasicFragment.java
@@ -1,17 +1,18 @@
 package com.espiandev.redux;
 
 import android.app.Activity;
+import android.app.Fragment;
 
-import com.espiandev.redux.ResourceStringProvider;
-import com.espiandev.redux.TitledFragment;
 import com.espiandev.redux.animation.AnimationFactory;
 import com.espiandev.redux.animation.AnimationFactoryProvider;
 import com.espiandev.redux.network.NetworkHelper;
 import com.espiandev.redux.network.NetworkHelperProvider;
 
-public abstract class BasicFragment extends TitledFragment implements ResourceStringProvider {
+public abstract class BasicFragment extends Fragment implements ResourceStringProvider, TitledItem {
     protected NetworkHelper networkHelper;
     protected AnimationFactory animationFactory;
+    protected TitleHost titleHost = TitleHost.NONE;
+    protected boolean hasSetTitle;
 
     @Override
     public void onAttach(Activity activity) {
@@ -22,5 +23,38 @@ public abstract class BasicFragment extends TitledFragment implements ResourceSt
         if (activity instanceof AnimationFactoryProvider) {
             animationFactory = ((AnimationFactoryProvider) activity).getAnimationFactory();
         }
+        if (activity instanceof TitleHost) {
+            titleHost = (TitleHost) activity;
+        }
+
+        hasSetTitle = true;
+        setTitles();
+    }
+
+    protected void setTitles() {
+        titleHost.setTitle(getHostedTitle(titleHost));
+
+        if (getHostedSubtitle(titleHost) != null) {
+            titleHost.setSubtitle(getHostedSubtitle(titleHost));
+        } else {
+            titleHost.setSubtitle("");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!hasSetTitle) {
+            setTitles();
+        }
+
+        hasSetTitle = false;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        titleHost = TitleHost.NONE;
     }
 }
