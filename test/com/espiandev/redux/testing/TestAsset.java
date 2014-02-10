2f4d23b1d0efd9cd211dff31dbeb1d04a87de64c
Alex Curran <aml.curran@gmail.com>
2014-02-10 11:18:34 +0000

    Fixed broken tests

--- test/com/espiandev/redux/testing/TestAsset.java
+++ test/com/espiandev/redux/testing/TestAsset.java
@@ -3,6 +3,8 @@ package com.espiandev.redux.testing;
 import com.espiandev.redux.Channel;
 import com.espiandev.redux.assets.Asset;
 
+import java.util.Date;
+
 public class TestAsset extends Asset {
 
     public static final long DURATION = 1000;
@@ -14,6 +16,11 @@ public class TestAsset extends Asset {
     @Override
     public long getDuration() {
         return DURATION;
+    }
+
+    @Override
+    public Date getBroadcastDate() {
+        return new Date(0);
     }
 
     @Override
