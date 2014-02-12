fcffbbdc22c770c695856c5311b9a8c3238a672f
Alex Curran <aml.curran@gmail.com>
2014-02-12 14:03:12 +0000

    Improve test coverage on Stacker

---
+++ test/com/espiandev/redux/navigation/FragmentManagerStackerTest.java
@@ -0,0 +1,75 @@
+package com.espiandev.redux.navigation;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+
+import com.espiandev.redux.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = "AndroidManifest.xml")
+public class FragmentManagerStackerTest {
+
+    private FragmentManagerStackerActivity activity;
+    private FragmentManagerStacker stacker;
+
+    @Before
+    public void setUp() {
+        ActivityController<FragmentManagerStackerActivity> activityController = Robolectric.buildActivity(FragmentManagerStackerActivity.class);
+        activity = activityController.create().get();
+        stacker = new FragmentManagerStacker(activity);
+    }
+
+    @Test
+    public void testPushFragment_SetsAFragmentOnTheFrame() {
+        Fragment fragment = new Fragment();
+
+        stacker.pushFragment(fragment);
+
+        assertEquals(fragment, getPoppedFragment());
+    }
+
+    @Test
+    public void testAddFragment_SetsAFragmentOnTheFrame() {
+        Fragment fragment = new Fragment();
+
+        stacker.addFragment(fragment);
+
+        assertEquals(fragment, getPoppedFragment());
+    }
+
+    @Test
+    public void testRemoveFragment_RemovesAFragmentOnTheFrame() {
+        Fragment fragment = new Fragment();
+
+        stacker.addFragment(fragment);
+        stacker.removeFragment();
+
+        assertNull(getPoppedFragment());
+    }
+
+    private Fragment getPoppedFragment() {
+        return activity.getFragmentManager().findFragmentById(R.id.host_frame);
+    }
+
+    private static class FragmentManagerStackerActivity extends Activity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+}
