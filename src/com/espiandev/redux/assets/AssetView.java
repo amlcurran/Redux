9b39921a586da7fff0e494abc6223e48c817f034
Alex Curran <aml.curran@gmail.com>
2014-04-21 00:07:15 +0100

    updates

---
+++ src/com/espiandev/redux/assets/AssetView.java
@@ -0,0 +1,16 @@
+package com.espiandev.redux.assets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class AssetView extends FrameLayout {
+
+    public AssetView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AssetView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
